63b9a4f59d721d2388f1002172eff1a9ecf2ce48
工藤隆弘 <>
2023-08-02 17:56:52 +0900

    refactor: terraform fmt

--- stg/subnets.tf
+++ stg/subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public_subnet_a" {
-  vpc_id            = aws_vpc.stg_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.stg_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "fanc_public_subnet_a_stg"
@@ -19,9 +20,11 @@ resource "aws_subnet" "private_subnet_a" {
 }
 
 resource "aws_subnet" "public_subnet_b" {
-  vpc_id            = aws_vpc.stg_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.stg_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
 
   tags = {
     Name = "fanc_public_subnet_b_stg"
