136deb6813edb617d681d83a0c88d4a9db178c5b
工藤隆弘 <>
2023-08-02 16:55:46 +0900

    chore : terraform fmt

--- stg/internet_gateway.tf
+++ stg/internet_gateway.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "stg_igw" {
   vpc_id = aws_vpc.stg_vpc.id
 
   tags = {
-    Name = "fanc-stg-igw"
+    Name        = "fanc-stg-igw"
     Environment = "staging"
   }
 }
