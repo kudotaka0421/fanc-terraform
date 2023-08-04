922540e6e78f7cf62f302b7ade5532d07a3bb93d
工藤隆弘 <>
2023-08-04 17:06:55 +0900

    Remove tfstate files from tracking

--- stg/security_group.tf
+++ stg/security_group.tf
@@ -21,3 +21,27 @@ resource "aws_security_group" "bastion" {
     Name = "allow_ssh_stg"
   }
 }
+
+resource "aws_security_group" "db" {
+  name        = "db"
+  description = "Allow MySQL inbound traffic"
+  vpc_id      = aws_vpc.stg_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["18.204.231.140/32", "60.38.109.203/32"] //[TODO]ここに<ECSのIPアドレス範囲>を追加する
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_mysql_stg"
+  }
+}
