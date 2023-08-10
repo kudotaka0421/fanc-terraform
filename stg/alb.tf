91f714afb7da72b31a46b0ee6c80b46746f84663
工藤隆弘 <>
2023-08-10 17:48:49 +0900

    feature : create alb.tf

---
+++ stg/alb.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "fanc_alb_stg" {
+  name               = "fanc-alb-stg"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_alb.id] # 追加のセキュリティグループを作成する場合
+  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+
+  enable_deletion_protection = true
+  enable_http2               = true
+  idle_timeout               = 60
+
+  tags = {
+    Name = "fanc-alb-stg"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.fanc_alb_stg.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fanc_stg.arn
+  }
+}
+
+resource "aws_lb_listener" "https_listener_stg" {
+  load_balancer_arn = aws_lb.fanc_alb_stg.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:us-east-1:757245745517:certificate/1532a906-f277-4dd3-9a1b-14c1ae07ab8a"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fanc_stg.arn
+  }
+}
--- stg/security_group.tf
+++ stg/security_group.tf
@@ -44,3 +44,37 @@ resource "aws_security_group" "db" {
     Name = "allow_mysql_stg"
   }
 }
+
+resource "aws_security_group" "allow_alb" {
+  name        = "allow_alb_stg"
+  description = "Allow ALB inbound traffic"
+  vpc_id      = aws_vpc.stg_vpc.id
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_alb_stg"
+  }
+}
+
